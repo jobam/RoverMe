e644bb7082afd6384b8e5de50d4fd4363215973c
Jonathan Abraham <jonathan.abraham@outlook.com>
2016-02-04 19:08:10 +0100

    Wifi direct libraryn socket done, wifi direct undone. Cannot build yet.

---
+++ Sources/Commun/RoverMeWifiDirect/SocketReaderWriter.cs
@@ -0,0 +1,125 @@
+﻿//*********************************************************
+//
+// Made by Abraham Jonathan
+//      04/02/2016
+//
+//*********************************************************
+
+using System;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using Windows.Devices.Enumeration;
+using System.ComponentModel;
+using Windows.Devices.WiFiDirect;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace RoverMeWifiDirect
+{
+    public static class Globals
+    {
+        public static readonly byte[] CustomOui = { 0xAA, 0xBB, 0xCC };
+        public static readonly byte CustomOuiType = 0xDD;
+        public static readonly byte[] WfaOui = { 0x50, 0x6F, 0x9A };
+        public static readonly byte[] MsftOui = { 0x00, 0x50, 0xF2 };
+        public static readonly string strServerPort = "50001";
+    }
+
+    public class SocketReaderWriter : IDisposable
+    {
+        #region Events to Subscribe
+
+        public delegate void ReceivedMessageDelegate(string receivedMessage);
+        public event ReceivedMessageDelegate ReceivedMessageEvent;
+
+        #endregion
+       
+        #region Properties
+
+        DataReader _dataReader;
+        DataWriter _dataWriter;
+        StreamSocket _streamSocket;
+        string _currentMessage;
+
+        #endregion
+
+        #region Cycle
+
+        public SocketReaderWriter(StreamSocket socket)
+        {
+            _dataReader = new DataReader(socket.InputStream);
+            _dataReader.UnicodeEncoding = UnicodeEncoding.Utf8;
+            _dataReader.ByteOrder = ByteOrder.LittleEndian;
+
+            _dataWriter = new DataWriter(socket.OutputStream);
+            _dataWriter.UnicodeEncoding = UnicodeEncoding.Utf8;
+            _dataWriter.ByteOrder = ByteOrder.LittleEndian;
+
+            _streamSocket = socket;
+            _currentMessage = null;
+        }
+
+        public void Dispose()
+        {
+            _dataReader.Dispose();
+            _dataWriter.Dispose();
+            _streamSocket.Dispose();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public async Task<bool> WriteMessage(string message)
+        {
+            try
+            {
+                _dataWriter.WriteUInt32(_dataWriter.MeasureString(message));
+                _dataWriter.WriteString(message);
+                await _dataWriter.StoreAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Socket closed: " + ex.Message);
+                return false;
+            }
+        }
+
+        public async void ReadMessage()
+        {
+            try
+            {
+                UInt32 bytesRead = await _dataReader.LoadAsync(sizeof(UInt32));
+                if (bytesRead > 0)
+                {
+                    // Determine how long the string is.
+                    UInt32 messageLength = _dataReader.ReadUInt32();
+                    bytesRead = await _dataReader.LoadAsync(messageLength);
+                    if (bytesRead > 0)
+                    {
+                        // Decode the string.
+                        _currentMessage = _dataReader.ReadString(messageLength);
+
+                        // raise event to tell message received
+                        ReceivedMessageEvent.Invoke(_currentMessage);
+
+                        ReadMessage();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Socket closed: " + ex.Message);
+            }
+        }
+
+        public string GetCurrentMessage()
+        {
+            return _currentMessage;
+        }
+    }
+
+    #endregion
+
+}
---
+++ Sources/Commun/RoverMeWifiDirect/WifiDirectServer.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.WiFiDirect;
+using Windows.Networking.Sockets;
+
+namespace RoverMeWifiDirect
+{
+    public class WifiDirectServer
+    {
+        #region Attributes
+        public ObservableCollection<ConnectedDevice> _connectedDevices
+        {
+            get;
+            private set;
+        }
+        WiFiDirectAdvertisementPublisher _publisher;
+        WiFiDirectConnectionListener _listener;
+        StreamSocketListener _listenerSocket;
+
+        #endregion
+
+        public WifiDirectServer()
+        {
+            _connectedDevices = new ObservableCollection<ConnectedDevice>();
+            _listenerSocket = null;
+        }
+
+
+        public void StartServer()
+        {
+            try
+            {
+                if (_publisher == null)
+                {
+                    _publisher = new WiFiDirectAdvertisementPublisher();
+                }
+
+                if (_listener == null)
+                {
+                    _listener = new WiFiDirectConnectionListener();
+                }
+
+                _listener.ConnectionRequested += OnConnectionRequested;
+
+                // can be defined to Intensive
+                _publisher.Advertisement.ListenStateDiscoverability = WiFiDirectAdvertisementListenStateDiscoverability.Normal;
+
+                _publisher.Start();
+
+                Debug.WriteLine("Advertisement started, waiting for StatusChanged callback...");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error starting Advertisement: " + ex.ToString());
+            }
+        }
+
+        #region Helpers
+
+        private async void OnConnectionRequested(WiFiDirectConnectionListener sender, WiFiDirectConnectionRequestedEventArgs connectionEventArgs)
+        {
+            try
+            {
+                var connectionRequest = connectionEventArgs.GetConnectionRequest();
+
+                    var tcsWiFiDirectDevice = new TaskCompletionSource<WiFiDirectDevice>();
+                    var wfdDeviceTask = tcsWiFiDirectDevice.Task;
+
+                    await Task.Run( async () =>
+                    {
+                        try
+                        {
+                            Debug.WriteLine("Connecting to " + connectionRequest.DeviceInformation.Name + "...");
+
+                            WiFiDirectConnectionParameters connectionParams = new WiFiDirectConnectionParameters();
+                            connectionParams.GroupOwnerIntent = Convert.ToInt16("0"); // must be set if mutiple devices ?
+
+                            // IMPORTANT: FromIdAsync needs to be called from the UI thread
+                            tcsWiFiDirectDevice.SetResult(await WiFiDirectDevice.FromIdAsync(connectionRequest.DeviceInformation.Id, connectionParams));
+                        }
+                        catch (Exception ex)
+                        {
+                            Debug.WriteLine("FromIdAsync task threw an exception: " + ex.ToString());
+                        }
+                    });
+
+                    WiFiDirectDevice wfdDevice = await wfdDeviceTask;
+
+                    // Register for the ConnectionStatusChanged event handler
+                    wfdDevice.ConnectionStatusChanged += OnConnectionStatusChanged;
+
+                    await Dispatcher.RunAsync(CoreDispatcherPriority.High, () =>
+                    {
+                        ConnectedDevice connectedDevice = new ConnectedDevice("Waiting for client to connect...", wfdDevice, null);
+                        _connectedDevices.Add(connectedDevice);
+                    });
+
+                    var EndpointPairs = wfdDevice.GetConnectionEndpointPairs();
+
+                    _listenerSocket = null;
+                    _listenerSocket = new StreamSocketListener();
+                    _listenerSocket.ConnectionReceived += OnSocketConnectionReceived;
+                    await _listenerSocket.BindEndpointAsync(EndpointPairs[0].LocalHostName, Globals.strServerPort);
+
+                    rootPage.NotifyUserFromBackground("Devices connected on L2, listening on IP Address: " + EndpointPairs[0].LocalHostName.ToString() +
+                                            " Port: " + Globals.strServerPort, NotifyType.StatusMessage);
+                }
+                else
+                {
+                    // Decline the connection request
+                    rootPage.NotifyUserFromBackground("Connection request from " + connectionRequest.DeviceInformation.Name + " was declined", NotifyType.ErrorMessage);
+                    connectionRequest.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                rootPage.NotifyUserFromBackground("Connect operation threw an exception: " + ex.Message, NotifyType.ErrorMessage);
+            }
+        }
+
+        #endregion
+
+    }
+}
