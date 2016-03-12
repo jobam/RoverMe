3cbb7f95fccc0e2040fa42530a5ed3a0ce4dfeba
Alexandre Nourissier <alexandre.nourissier@outlook.com>
2016-03-12 15:45:20 +0100

    refactor(*):Une seule solution, c'est quand même mieux. J'ai intégrér le code de Jonathan et fait un projet de tests unitaire avec XUnit pour plus tard

---
+++ Sources/RoverMe/RoverMe.Shared.Commands/MovementCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoverMe.Shared.Commands
+{
+    public class MovementCommand
+    {
+        #region Properties
+
+        private decimal magnitude;
+
+        /// <summary>
+        /// Represent the percentage of force applied in the movement
+        /// </summary>
+        public decimal Magnitude
+        {
+            get { return magnitude; }
+            set { magnitude = value; }
+        }
+
+        private decimal angularSpeed;
+
+        /// <summary>
+        /// Represent the percentage of rotational speed applied in the movement
+        /// </summary>
+        public decimal AngularSpeed
+        {
+            get { return angularSpeed; }
+            set { angularSpeed = value; }
+        }
+
+        #endregion
+
+        #region Constructor
+        public MovementCommand()
+        {
+        }
+        public MovementCommand(decimal magnitude, decimal angularSpeed)
+        {
+            this.Magnitude = magnitude;
+            this.AngularSpeed = angularSpeed;
+        }
+        #endregion
+
+    }
+}
