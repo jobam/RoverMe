7eaee806852c63a947a2976d7a4b21ff788f90b7
Jonathan Abraham <Jonathan Abraham>
2016-05-13 18:53:07 +0200

    gestion des servos moteurs base

--- Sources/RoverMe/RoverMe.Robot.HostApp/Properties/AssemblyInfo.cs
+++ Sources/RoverMe/RoverMe.Robot.HostApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RoverMe.Robot.RaspberryPi2")]
+[assembly: AssemblyTitle("RoverMe.Robot.HostApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RoverMe.Robot.RaspberryPi2")]
+[assembly: AssemblyProduct("RoverMe.Robot.HostApp")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
