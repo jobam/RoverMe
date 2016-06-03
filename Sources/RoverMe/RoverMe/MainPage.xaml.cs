9d0a0ed4299a1f5faf06cbef69f3f55a6fe8052e
DarkCenobyte <darkcenobyte@gmail.com>
2016-06-03 16:19:10 +0200

    Start working on calling the controller from the interface UI.

--- Sources/RoverMe/RoverMe/MainPage.xaml.cs
+++ Sources/RoverMe/RoverMe/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using RoverMe.Shared.Commands;
+using RoverMe.Shared.Network;
 
 // Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,14 +24,59 @@ namespace RoverMeClient
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private ControllerCommand controllerCommand;
+        private SocketClient socketClient;
         public MainPage()
         {
             this.InitializeComponent();
+
         }
 
         private void forwardButton(System.Object sender, RoutedEventArgs e)
         {
+            controllerCommand.FowardCommand(new[] {"1","1"});
+        }
 
+        private void rightButton(object sender, RoutedEventArgs e)
+        {
+            controllerCommand.RightCommand(new[] { "1", "1" });
+        }
+
+        private void leftButton(object sender, RoutedEventArgs e)
+        {
+            controllerCommand.LeftCommand(new[] { "1", "1" });
+        }
+
+        private void backwardButton(object sender, RoutedEventArgs e)
+        {
+            controllerCommand.BackwardCommand(new[] { "1", "1" });
+        }
+
+        private void forwardRightButton(object sender, RoutedEventArgs e)
+        {
+            controllerCommand.FowardRightCommand(new[] { "1", "1" });
+        }
+
+        private void forwardLeftButton(object sender, RoutedEventArgs e)
+        {
+            controllerCommand.FowardLeftCommand(new[] { "1", "1" });
+        }
+
+        private void backwardRightButton(object sender, RoutedEventArgs e)
+        {
+            controllerCommand.BackwardRightCommand(new[] { "1", "1" });
+        }
+
+        private void backwardLeftButton(object sender, RoutedEventArgs e)
+        {
+            controllerCommand.BackwardLeftCommand(new[] { "1", "1" });
+        }
+
+        private void connectionButton(object sender, RoutedEventArgs e)
+        {
+            socketClient = SocketClient.Instance;
+            
+            //controllerCommand = socketClient.Connect.controllerCommand;
         }
     }
 }
