9ae32c3ddeea04a568c406f2c05ffceafe7d94c2
jonathan <jonathan.abraham@outlook.com>
2016-06-08 16:18:21 +0200

    les moteurs tournent. A suivre modifier le motor controller

--- Sources/RoverMe/RoverMe.Robot.HostApp/MainPage.xaml.cs
+++ Sources/RoverMe/RoverMe.Robot.HostApp/MainPage.xaml.cs
@@ -39,12 +39,30 @@ namespace RoverMe.Robot.HostApp
             //MotorControl.MoveMotorsForTime(3000);
 
             GpioController gpio = GpioController.GetDefault();
-            var pin = gpio.OpenPin(22);
-            pin.SetDriveMode(GpioPinDriveMode.Output);
+            var pin1 = gpio.OpenPin(23);
+            var pin2 = gpio.OpenPin(24);
+            var pin3 = gpio.OpenPin(25);
+
+            var pin21 = gpio.OpenPin(4);// 4
+            var pin22 = gpio.OpenPin(5);// 5
+            var pin23 = gpio.OpenPin(6);// 6
+
             while (true)
             {
-                pin.Write(GpioPinValue.High);
-               await Task.Delay(500);
+                pin1.Write(GpioPinValue.High);
+                pin2.Write(GpioPinValue.Low);
+                pin3.Write(GpioPinValue.High);
+                pin21.Write(GpioPinValue.High);
+                pin22.Write(GpioPinValue.Low);
+                pin23.Write(GpioPinValue.High);
+
+                pin1.SetDriveMode(GpioPinDriveMode.Output);
+                pin2.SetDriveMode(GpioPinDriveMode.Output);
+                pin3.SetDriveMode(GpioPinDriveMode.Output);
+                pin21.SetDriveMode(GpioPinDriveMode.Output);
+                pin22.SetDriveMode(GpioPinDriveMode.Output);
+                pin23.SetDriveMode(GpioPinDriveMode.Output);
+                await Task.Delay(1000);
             }
 
             //while (true)
